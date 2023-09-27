73ec895acdf5a6fc155cb4393e612409e7ff45bf
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-09-27 21:51:42 +0500

    Nothinf

--- web/settings.py
+++ web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3yb5@eukne6=3y3yf8$wewm27nw&-ult^b6a^uadzrg3$1i^d!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','.vercel.app', '.now.sh']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static',]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
