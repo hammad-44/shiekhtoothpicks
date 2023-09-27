231091c0f9aa6b6e4fca192183cfca4f30f89c20
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-09-27 17:51:44 +0500

    Update urls.py

--- web/urls.py
+++ web/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('toothpicks.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
