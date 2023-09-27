becff714ac71a96b93cdf185cdaa4d847361f7c8
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-09-27 17:44:14 +0500

    for cervel

--- web/settings.py
+++ web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3yb5@eukne6=3y3yf8$wewm27nw&-ult^b6a^uadzrg3$1i^d!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*','.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,  "static/")
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
