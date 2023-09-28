9dc3ba2ee8878fd28c10356761ee2041d7e4d0bf
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-09-28 17:10:53 +0500

    renew

--- toothpicks/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import product,Contact,Order
-# Register your models here.
-admin.site.register(product)
-admin.site.register(Contact)
-admin.site.register(Order)
--- toothpicks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToothpicksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'toothpicks'
