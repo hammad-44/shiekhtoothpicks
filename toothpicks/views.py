8c73a1d34ae12fe720a8b21775cd16d435a8d3c8
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-09-29 21:58:28 +0500

    Updated ORder

--- toothpicks/views.py
+++ toothpicks/views.py
@@ -28,12 +28,18 @@ def order(request):
         product = request.POST['product']
         quantity = request.POST['quantity']
         message = request.POST['message']
-        if int(quantity) < 50 and product == "ToothPicks" or product == "Printed ToothPicks":
+
+        if int(quantity) < 50 and product == "ToothPicks":
             messages.error(request,"You Have to Order Atleast 50 Boxes")
+            return render(request, 'orderform.html')
         elif int(quantity) < 10000 and product == "Sugar Sachets":
             messages.error(request,"You Have to Order Atleast 10,000 Pieces")
-        else:
-            url =f"https://wa.me/923034139181?text=Name : {name}%0aEmail : {email}%0aPhone Number : {phoneNumber}%0aProduct : {product}%0aNumber of Box : {quantity}%0aMessage : {message}"
-            webbrowser.open(url)
-            return render(request, "order.html")
-    return render(request, 'orderform.html')
+            return render(request, 'orderform.html')
+        elif int(quantity) < 10000 and product == "Printed ToothPicks":
+            messages.error(request,"You Have to Order Atleast 10,000 Pieces")
+            return render(request, 'orderform.html')
+        
+        url =f"https://wa.me/923034139181?text=Name : {name}%0aEmail : {email}%0aPhone Number : {phoneNumber}%0aProduct : {product}%0aNumber of Box : {quantity}%0aMessage : {message}"
+        webbrowser.open(url)
+        return render(request, "order.html")
+   
