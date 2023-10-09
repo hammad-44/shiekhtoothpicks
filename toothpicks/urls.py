eab879a18a9a95b8967ac1027280b4b0346f3cd5
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-10-09 13:16:52 +0500

    Orde and contac

--- toothpicks/urls.py
+++ toothpicks/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('contact',views.contact, name='contact'),
-    path('order',views.order, name='order'),
     path('orderform',views.orderform, name='orderform'),
     path('login',views.login, name='login'),
     path('checkorders',views.checkorders, name='checkorders')
+    
 ]
     
--- toothpicks/views.py
+++ toothpicks/views.py
@@ -1,23 +1,6 @@
 from django.shortcuts import render,redirect
-import webbrowser
 from django.contrib import messages
-from django.http import HttpResponseRedirect
 from django import forms
-from django.core.exceptions import ViewDoesNotExist
-
-class RedirectException(BaseException):
-    def __init__(self, url):
-        self.url = url
-
-form_data_list = []
-class MyForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    phone = forms.CharField()
-    email = forms.EmailField()
-    phoneNumber = forms.CharField()
-    product = forms.CharField()
-    quantity = forms.CharField()
-    message = forms.CharField()
 
 
 
@@ -33,11 +16,8 @@ def checkorders(request):
         password = request.POST["password"]
         if username == "Sheikh Hanfi" and password == "7bugstoothpicks":
             
-            context = {
-                'form_data_list': form_data_list,
-            }
-            print(context)
-            return render(request,"orderslist.html",context)
+            # print(context)
+            return render(request,"orderslist.html")
         else:
             return render(request, "login.html")
 
@@ -48,8 +28,8 @@ def contact(request):
         phoneNumber = request.POST['phone']
         message = request.POST['message']
         url =f"https://wa.me/923034139181?text=Name : {name}%0aPhone Number : {phoneNumber}%0aMessage : {message}"
-        webbrowser.open(url)
-        return render(request, 'retur.html')
+
+        return redirect(url)
     else:
         return render(request,'contact.html')
 
@@ -66,7 +46,6 @@ def order(request):
         product = request.POST['product']
         quantity = request.POST['quantity']
         message = request.POST['message']
-        form = MyForm(request.POST)
 
         if int(quantity) < 50 and product == "ToothPicks":
             messages.error(request,"You Have to Order Atleast 50 Boxes")
@@ -78,11 +57,9 @@ def order(request):
             messages.error(request,"You Have to Order Atleast 10,000 Pieces")
             return render(request, 'orderform.html')
         formdata = {"name":name,"email":email,"phoneNumber":phoneNumber,"product":product,"quantity":quantity,"message":message}
-        form_data_list.append(formdata)
-        print(form_data_list)
-        url =f"https://wa.me/923034139181?text=Name : {name}%0aEmail : {email}%0aPhone Number : {phoneNumber}%0aProduct : {product}%0aNumber of Box : {quantity}%0aMessage : {message}"
-        webbrowser.open_new_tab(url)
-        return render(request, "order.html")
+        url =f"https://wa.me/923034139181?text=Name : {name}%0aEmail : {email}%0aPhone Number : {phoneNumber}%0aProduct : {product}%0aNumber of Box : {quantity}%0aAdress : {message}"
+
+        return redirect(url)
         
 
    
