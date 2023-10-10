10a359dc1caca397f23b1dfbae10fb35f9a1a683
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-10-11 02:34:11 +0500

    final db

--- toothpicks/models.py
+++ toothpicks/models.py
@@ -7,16 +7,16 @@ class product(models.Model):
     name=models.CharField(max_length=255)
     desc=models.CharField(max_length=255)
     price=models.CharField(max_length=30)
-    img = models.ImageField(upload_to='static/products/',storage=FileSystemStorage())
+    img = models.TextField() 
     def __str__(self):
-        return f"Product #{self.sno} - {self.name}- {timezone.now()}"
+        return f"Product NO {self.sno} - {self.name}"
 class Contacst(models.Model):
     sno=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
     email=models.CharField(max_length=255)
     message=models.CharField(max_length=255)
     def __str__(self):
-        return f"Contact #{self.sno} - {self.name}- {timezone.now()}"
+        return f"Contact No {self.sno} - {self.name}- {timezone.now()}"
 class Order(models.Model):
     sno=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
@@ -27,4 +27,4 @@ class Order(models.Model):
     message=models.TextField(max_length=255)
     date = models.DateField(auto_now=True)
     def __str__(self):
-        return f"Order #{self.sno} - {self.name}- {timezone.now()}"
+        return f"Order No {self.sno} - {self.name}- {timezone.now()}"
